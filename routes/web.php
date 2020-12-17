35d842d7021e4cddb24b9436f8bfa0351a6531a9
Abdalqader <alqaderabd@gmail.com>
2020-12-17 05:00:54 +0300

    Admin Authentication

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,8 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/admin', 'admin.dashboard.index');
+Auth::routes();
 
+require 'admin.php';
 
 Route::get('/', function () {
     return view('welcome');
