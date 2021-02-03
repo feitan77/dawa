a915a3d4813434fc9758b46ebfdbc01ed26592b1
Abdalqader <alqaderabd@gmail.com>
2021-02-03 17:22:58 +0300

    redirect after store

--- resources/views/admin/app.blade.php
+++ resources/views/admin/app.blade.php
@@ -5,8 +5,8 @@
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="stylesheet" type="text/css" href="{{ asset('backend/css/main.css', true) }}" />
-    <link rel="stylesheet" type="text/css" href="{{ asset('backend/css/font-awesome/4.7.0/css/font-awesome.min.css', true) }}"/>
+    <link rel="stylesheet" type="text/css" href="{{ asset('backend/css/main.css') }}" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('backend/css/font-awesome/4.7.0/css/font-awesome.min.css') }}"/>
 </head>
 <body class="app sidebar-mini rtl">
 @include('admin.partials.header')
