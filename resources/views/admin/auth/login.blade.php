1f7bc762b0870aa9c1449c48d0abb0eab7021c48
Abdalqader <alqaderabd@gmail.com>
2021-02-03 21:06:27 +0300

    fixing a url

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -4,8 +4,8 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="{{ asset('/public/backend/css/main.css') }}" />
-    <link rel="stylesheet" type="text/css" href="{{ asset('/public/backend/css/font-awesome/4.7.0/css/font-awesome.min.css') }}"/>
+    <link rel="stylesheet" type="text/css" href="{{ asset('/backend/css/main.css') }}" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('/backend/css/font-awesome/4.7.0/css/font-awesome.min.css') }}"/>
     <title>Login - {{ config('app.name') }}</title>
 </head>
 <body>
